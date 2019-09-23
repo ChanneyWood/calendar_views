6c47c79f5f95ae48a24920e94842366ad10cf41a
liliang059 <liliang059@ke.com>
2019-09-23 15:26:52 +0800

    add myself

--- example/lib/day_view_example.dart
+++ example/lib/day_view_example.dart
@@ -13,32 +13,37 @@ class Event {
     @required this.title,
   });
 
+  bool visible = true;
   final int startMinuteOfDay;
   final int duration;
 
   final String title;
 }
 
-List<Event> eventsOfDay0 = <Event>[
+bool onlyVisible = false;
+int visibleIndex = -1;
+
+List<Event> events = <Event>[
   new Event(startMinuteOfDay: 0, duration: 60, title: "Midnight Party"),
   new Event(
       startMinuteOfDay: 6 * 60, duration: 2 * 60, title: "Morning Routine"),
   new Event(startMinuteOfDay: 6 * 60, duration: 60, title: "Eat Breakfast"),
   new Event(startMinuteOfDay: 7 * 60, duration: 60, title: "Get Dressed"),
+  new Event(startMinuteOfDay: 6 * 60, duration: 60, title: "Get Dressed well"),
   new Event(
       startMinuteOfDay: 18 * 60, duration: 60, title: "Take Dog For A Walk"),
 ];
 
-List<Event> eventsOfDay1 = <Event>[
-  new Event(startMinuteOfDay: 1 * 60, duration: 90, title: "Sleep Walking"),
-  new Event(startMinuteOfDay: 7 * 60, duration: 60, title: "Drive To Work"),
-  new Event(startMinuteOfDay: 8 * 60, duration: 20, title: "A"),
-  new Event(startMinuteOfDay: 8 * 60, duration: 30, title: "B"),
-  new Event(startMinuteOfDay: 8 * 60, duration: 60, title: "C"),
-  new Event(startMinuteOfDay: 8 * 60 + 10, duration: 20, title: "D"),
-  new Event(startMinuteOfDay: 8 * 60 + 30, duration: 30, title: "E"),
-  new Event(startMinuteOfDay: 23 * 60, duration: 60, title: "Midnight Snack"),
-];
+//List<Event> eventsOfDay1 = <Event>[
+//  new Event(startMinuteOfDay: 1 * 60, duration: 90, title: "Sleep Walking"),
+//  new Event(startMinuteOfDay: 7 * 60, duration: 60, title: "Drive To Work"),
+//  new Event(startMinuteOfDay: 8 * 60, duration: 20, title: "A"),
+//  new Event(startMinuteOfDay: 8 * 60, duration: 30, title: "B"),
+//  new Event(startMinuteOfDay: 8 * 60, duration: 60, title: "C"),
+//  new Event(startMinuteOfDay: 8 * 60 + 10, duration: 20, title: "D"),
+//  new Event(startMinuteOfDay: 8 * 60 + 30, duration: 30, title: "E"),
+//  new Event(startMinuteOfDay: 23 * 60, duration: 60, title: "Midnight Snack"),
+//];
 
 class DayViewExample extends StatefulWidget {
   @override
@@ -47,14 +52,13 @@ class DayViewExample extends StatefulWidget {
 
 class _DayViewExampleState extends State<DayViewExample> {
   DateTime _day0;
-  DateTime _day1;
+  //DateTime _day1;
 
   @override
   void initState() {
     super.initState();
 
     _day0 = new DateTime.now();
-    _day1 = _day0.toUtc().add(new Duration(days: 1)).toLocal();
   }
 
   String _minuteOfDayToHourMinuteString(int minuteOfDay) {
@@ -64,29 +68,37 @@ class _DayViewExampleState extends State<DayViewExample> {
   }
 
   List<StartDurationItem> _getEventsOfDay(DateTime day) {
-    List<Event> events;
-    if (day.year == _day0.year &&
-        day.month == _day0.month &&
-        day.day == _day0.day) {
-      events = eventsOfDay0;
+    if (onlyVisible) {
+      List<StartDurationItem> list = [];
+      Event event = events[visibleIndex];
+      list.add(StartDurationItem(
+          startMinuteOfDay: event.startMinuteOfDay,
+          duration: event.duration,
+          builder: (context, itemPosition, itemSize) => _eventBuilder(
+                context,
+                itemPosition,
+                itemSize,
+                event,
+                events.indexOf(event),
+              )));
+      return list;
     } else {
-      events = eventsOfDay1;
+      return events
+          .map(
+            (event) => new StartDurationItem(
+                  startMinuteOfDay: event.startMinuteOfDay,
+                  duration: event.duration,
+                  builder: (context, itemPosition, itemSize) => _eventBuilder(
+                        context,
+                        itemPosition,
+                        itemSize,
+                        event,
+                        events.indexOf(event),
+                      ),
+                ),
+          )
+          .toList();
     }
-
-    return events
-        .map(
-          (event) => new StartDurationItem(
-                startMinuteOfDay: event.startMinuteOfDay,
-                duration: event.duration,
-                builder: (context, itemPosition, itemSize) => _eventBuilder(
-                      context,
-                      itemPosition,
-                      itemSize,
-                      event,
-                    ),
-              ),
-        )
-        .toList();
   }
 
   @override
@@ -99,7 +111,6 @@ class _DayViewExampleState extends State<DayViewExample> {
         properties: new DayViewProperties(
           days: <DateTime>[
             _day0,
-            _day1,
           ],
         ),
         child: new Column(
@@ -111,26 +122,45 @@ class _DayViewExampleState extends State<DayViewExample> {
               ),
             ),
             new Expanded(
-              child: new SingleChildScrollView(
-                child: new DayViewSchedule(
-                  heightPerMinute: 1.0,
-                  components: <ScheduleComponent>[
-                    new TimeIndicationComponent.intervalGenerated(
-                      generatedTimeIndicatorBuilder:
-                          _generatedTimeIndicatorBuilder,
+              child: Stack(
+                children: <Widget>[
+                  Row(
+                    children: <Widget>[
+                      new SingleChildScrollView(
+                        child: new DayViewSchedule(
+                          heightPerMinute: 1.0,
+                          components: <ScheduleComponent>[
+                            new TimeIndicationComponent.intervalGenerated(
+                              generatedTimeIndicatorBuilder:
+                              _generatedTimeIndicatorBuilder,
+                            ),
+                            new SupportLineComponent.intervalGenerated(
+                              generatedSupportLineBuilder:
+                              _generatedSupportLineBuilder,
+                            ),
+                            new DaySeparationComponent(
+                              generatedDaySeparatorBuilder:
+                              _generatedDaySeparatorBuilder,
+                            ),
+                            new EventViewComponent(
+                              getEventsOfDay: _getEventsOfDay,
+                            ),
+                          ],
+                        ),
+                      )
+                    ],
+                  ),
+                  Container(
+                    width: 80,
+                    color: Color.fromARGB(155, 255, 255, 255),
+                    child: Stack(
+                      children: <Widget>[
+                        _timeCircle(events[visibleIndex],true),
+                        _timeCircle(events[visibleIndex],false)
+                      ],
                     ),
-                    new SupportLineComponent.intervalGenerated(
-                      generatedSupportLineBuilder: _generatedSupportLineBuilder,
-                    ),
-                    new DaySeparationComponent(
-                      generatedDaySeparatorBuilder:
-                          _generatedDaySeparatorBuilder,
-                    ),
-                    new EventViewComponent(
-                      getEventsOfDay: _getEventsOfDay,
-                    ),
-                  ],
-                ),
+                  )
+                ],
               ),
             ),
           ],
@@ -186,7 +216,7 @@ class _DayViewExampleState extends State<DayViewExample> {
       width: itemWidth,
       child: new Container(
         height: 0.7,
-        color: Colors.grey[700],
+        color: Color.fromARGB((255 * 0.4).floor(), 228, 231, 240),
       ),
     );
   }
@@ -216,18 +246,93 @@ class _DayViewExampleState extends State<DayViewExample> {
     ItemPosition itemPosition,
     ItemSize itemSize,
     Event event,
+    int index,
   ) {
     return new Positioned(
       top: itemPosition.top,
       left: itemPosition.left,
       width: itemSize.width,
       height: itemSize.height,
-      child: new Container(
-        margin: new EdgeInsets.only(left: 1.0, right: 1.0, bottom: 1.0),
-        padding: new EdgeInsets.all(3.0),
-        color: Colors.green[200],
-        child: new Text("${event.title}"),
+//      child: Column(
+//        children: <Widget>[
+//          onlyVisible?GestureDetector(
+//            onPanUpdate: (DragUpdateDetails e){
+//              print("move");
+//              _moveLine(index,e,itemSize.height);
+//            },
+//            child: Container(
+//              height: 3,
+//              color: Colors.blue,
+//            ),
+//          ):Container(),
+      child: GestureDetector(
+        child: new Container(
+          margin: new EdgeInsets.only(left: 1.0, right: 1.0, bottom: 1.0),
+          padding: new EdgeInsets.all(3.0),
+          color: Color.fromARGB(55, 165, 214, 167),
+          child: new Text("${event.title}"),
+        ),
+        onTap: () {
+          setState(() {
+            if (onlyVisible) {
+              _changeOnlyVisible(index, false);
+            } else {
+              _changeOnlyVisible(index, true);
+            }
+          });
+        },
       ),
+//          onlyVisible?GestureDetector(
+//            onPanDown: (DragDownDetails e){
+//              print("用户手按下");
+//            },
+//            onPanUpdate: (DragUpdateDetails e){
+//              print("move");
+//              _moveLine(index,e,itemSize.height);
+//            },
+//            onPanEnd: (DragEndDetails e){
+//              print("抬起");
+//            },
+//            child: Container(
+//              height: 3,
+//              color: Colors.blue,
+//            ),
+//          ):Container(),
+//        ],
+//      ),
     );
+  }
+
+  void _moveLine(int index, DragUpdateDetails e, double height) {
+    setState(() {
+      Event newEvent = new Event(
+          startMinuteOfDay: events[index].startMinuteOfDay,
+          duration:
+              (events[index].duration + (e.delta.dy / height) * 60).floor(),
+          title: events[index].title);
+      events[index] = newEvent;
+    });
+  }
+
+  _timeCircle(Event event,bool start) {
+    double timeShow = (start ? event.startMinuteOfDay  : event.startMinuteOfDay + event.duration) as double;
+    String time =
+    return Positioned(
+      top: timeShow,
+      left: 0,
+      child: CircleAvatar(
+        child: Text("01.00"),
+      ),
+    )
+  }
+}
+
+void _changeOnlyVisible(int index, bool only) {
+  if (only) {
+    onlyVisible = true;
+    visibleIndex = index;
+  } else {
+    onlyVisible = false;
+    visibleIndex = -1;
   }
 }
